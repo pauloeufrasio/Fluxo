081a688169433a9dbe6ee8ce5d2134ec8664bfaa
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-03-30 07:05:47 -0300

    try catch

--- src/ContaPoupanca.java
+++
@@ -1,7 +0,0 @@
-public class ContaPoupanca extends Conta{
-    // Criando construtor especifico dessa classe atribuirdo parametro agencia e numero
-    public ContaPoupanca(int agencia, int numero){ // Chamando classe mãe
-        super(agencia, numero);
-    }
-
-}
--- src/fluxo.java
+++ src/fluxo.java
@@ -1,22 +1,29 @@
 public class fluxo {
 
     public static void main(String[] args) {
-        //"Ini do main", acessaremos o metodo1() através da chamada; //"Ini do main", acessaremos o metodo1() através da chamada;
         System.out.println("Ini do main");
-        metodo1();
+        try {
+            metodo1();
+        } catch(ArithmeticException | NullPointerException ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
+        }
         System.out.println("Fim do main");
     }
-    // "Ini do metodo1", acessaremos o metodo2();
+
     private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
-    // Ini do metodo2", na sequência, será executado um laço no qual a variável i começa em 1 e vai até 5. Essa mensagem será exibida até o final do laço.
+
     private static void metodo2() {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            Conta c = null;
+            c.deposita();
         }
         System.out.println("Fim do metodo2");
     }
