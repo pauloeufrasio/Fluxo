9ecb3340128f41c4ee852f182e9d4aaa39ede645
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-03-30 06:35:11 -0300

    acessaremos o metodos

--- src/fluxo.java
+++ src/fluxo.java
@@ -1,11 +1,12 @@
 public class fluxo {
 
     public static void main(String[] args) {
+        //"Ini do main", acessaremos o metodo1() através da chamada; //"Ini do main", acessaremos o metodo1() através da chamada;
         System.out.println("Ini do main");
         metodo1();
         System.out.println("Fim do main");
     }
-    //"Ini do main", acessaremos o metodo1() através da chamada;
+    // "Ini do metodo1", acessaremos o metodo2();
     private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
