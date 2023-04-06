275463d47d398761ce1dc98bb5fa5ba93dd13c59
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-04-06 01:17:16 -0300

    Lançando exeções

--- src/fluxo.java
+++ src/fluxo.java
@@ -1,7 +1,9 @@
 public class fluxo {
 
     public static void main(String[] args) {
+        //Fluxo de execução
         System.out.println("Ini do main");
+        // Exceção implementada
         try {
             metodo1();
         } catch(ArithmeticException | NullPointerException ex) {
@@ -11,12 +13,14 @@ public class fluxo {
         }
         System.out.println("Fim do main");
     }
+    // Depois do metodo1 ele executa o metodo 2
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
+    // executando metodo2 agora ele retora pro metodo 1 e printa o fim do main de cada metodo
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
