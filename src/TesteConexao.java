f5301ace70545f019d4180deab8f37efa066f8e2
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-04-09 17:00:01 -0300

    finally

--- src/TesteConexao.java
+++ src/TesteConexao.java
@@ -1,19 +1,28 @@
 public class TesteConexao {
 
     public static void main(String[] args) {
-
-        Conexao con = null;
-
-        try {
-            con = new Conexao();
-            con.leDados();
+        try (Conexao conexao = new Conexao()) {
+            conexao.leDados();
         } catch (IllegalStateException ex) {
-            System.out.println("Deu erro na conexão");
-            // finally, um bloco opcional que podemos colocar no final e que sempre será executado, com ou sem erro.
-        } finally {
-            con.fecha();
+            System.out.println("Deu Erro na conexão");
+            {
 
+                //-----------------------------------------------------------------------
+
+//        Conexao con = null;
+//
+//        try {
+//            con = new Conexao();
+//            con.leDados();
+//        } catch (IllegalStateException ex) {
+//            System.out.println("Deu erro na conexão");
+//            // finally, um bloco opcional que podemos colocar no final e que sempre será executado, com ou sem erro.
+//        } finally {
+//            con.fecha();
+//
+//        }
+
+            }
         }
-
     }
 }
